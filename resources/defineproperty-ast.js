8cc4a98450305050b8acbb06d5d696ce3d4c88cd
fedor_kolbeshin <fa.kolbeshin@tensor.ru>
2020-07-16 23:16:40 +0300

    added lazy initialization of packed libraries https://online.sbis.ru/opendoc.html?guid=02819f8b-eaa5-44ef-84a2-d46bb8f41497

---
+++ resources/defineproperty-ast.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = {
+   type: 'ExpressionStatement',
+   expression: {
+      type: 'CallExpression',
+      callee: {
+         type: 'MemberExpression',
+         computed: false,
+         object: {
+            type: 'Identifier',
+            name: 'Object'
+         },
+         property: {
+            type: 'Identifier',
+            name: 'defineProperty'
+         }
+      },
+      arguments: [
+         {
+            type: 'Identifier',
+            name: 'exports'
+         },
+         {
+            type: 'Literal',
+            value: '<name of your property>',
+            raw: '"<name of your property>"'
+         },
+         {
+            type: 'ObjectExpression',
+            properties: [
+               {
+                  type: 'Property',
+                  key: {
+                     type: 'Identifier',
+                     name: 'get'
+                  },
+                  computed: false,
+                  value: {
+                     type: 'FunctionExpression',
+                     id: null,
+                     params: [],
+                     body: {
+                        type: 'BlockStatement',
+                        body: [
+                           {
+                              type: 'ReturnStatement',
+                              argument: {
+                                 type: 'Identifier',
+                                 name: 'variable'
+                              }
+                           }
+                        ]
+                     },
+                     generator: false,
+                     expression: false,
+                     async: false
+                  },
+                  kind: 'init',
+                  method: false,
+                  shorthand: false
+               }
+            ]
+         }
+      ]
+   }
+};
--- test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/testNativeNamesImports.modulepack.js
+++ test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/testNativeNamesImports.modulepack.js
@@ -4,32 +4,41 @@ define('Modul/testNativeNamesImports', [
 ], function (require, exports) {
     Object.defineProperty(exports, '__esModule', { value: true });
         exports['Modul/_es6/fetch'] = true;
-        var Modul__es6_fetch = function () {
-        'use strict';
-        var exports = {};
-        var result = function (require, exports) {
-            'use strict';
-            Object.defineProperty(exports, '__esModule', { value: true });
-            function someTest() {
-                var test1 = 'Тестовое сообщение';
-                return test1;
-            }
-            exports.someTest = someTest;
-        }(require, exports);
-        if (result instanceof Function) {
-            return result;
-        } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
-            return result;
-        } else {
-            for (var property in result) {
-                if (result.hasOwnProperty(property)) {
-                    exports[property] = result[property];
+        var Modul__es6_fetch;
+    var Modul__es6_fetch_func = function () {
+        if (!Modul__es6_fetch) {
+            Modul__es6_fetch = function () {
+                'use strict';
+                var exports = {};
+                var result = function (require, exports) {
+                    'use strict';
+                    Object.defineProperty(exports, '__esModule', { value: true });
+                    function someTest() {
+                        var test1 = 'Тестовое сообщение';
+                        return test1;
+                    }
+                    exports.someTest = someTest;
+                }(require, exports);
+                if (result instanceof Function) {
+                    return result;
+                } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
+                    return result;
+                } else {
+                    for (var property in result) {
+                        if (result.hasOwnProperty(property)) {
+                            exports[property] = result[property];
+                        }
+                    }
                 }
-            }
+                return exports;
+            }();
         }
-        return exports;
-    }();
-    exports.fetch = Modul__es6_fetch;
-    
+        return Modul__es6_fetch;
+    };
+    Object.defineProperty(exports, 'fetch', {
+        get: function () {
+            return Modul__es6_fetch_func();
+        }
+    });
     return exports;
 });
