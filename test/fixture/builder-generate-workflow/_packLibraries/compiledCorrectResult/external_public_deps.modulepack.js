86f4a394bf62b846a3e1107cbbdc1067129e9a6a
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-07-24 16:48:21 +0300

    added lazy initialization of packed libraries https://online.sbis.ru/opendoc.html?guid=02819f8b-eaa5-44ef-84a2-d46bb8f41497

--- test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/external_public_deps.modulepack.js
+++ test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/external_public_deps.modulepack.js
@@ -6,31 +6,36 @@ define('Modul/external_public_deps', [
     'Modul/publicFunction1'
 ], function (removeArrayDuplicates, require, exports, Module_1, testFunction_1) {
         exports['Modul/_es6/testPublicModule'] = true;
-        var Modul__es6_testPublicModule = function () {
-        'use strict';
-        var exports = {};
-        var result = function (require, exports, removeArrayDuplicates) {
-            'use strict';
-            return removeArrayDuplicates;
-        }(require, exports, removeArrayDuplicates);
-        if (result instanceof Function) {
-            return result;
-        } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
-            return result;
-        } else {
-            for (var property in result) {
-                if (result.hasOwnProperty(property)) {
-                    exports[property] = result[property];
+        var Modul__es6_testPublicModule;
+    var Modul__es6_testPublicModule_func = function () {
+        if (!Modul__es6_testPublicModule) {
+            Modul__es6_testPublicModule = function () {
+                'use strict';
+                var exports = {};
+                var result = function (require, exports, removeArrayDuplicates) {
+                    'use strict';
+                    return removeArrayDuplicates;
+                }(require, exports, removeArrayDuplicates);
+                if (result instanceof Function) {
+                    return result;
+                } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
+                    return result;
+                } else {
+                    for (var property in result) {
+                        if (result.hasOwnProperty(property)) {
+                            exports[property] = result[property];
+                        }
+                    }
                 }
-            }
+                return exports;
+            }();
         }
-        return exports;
-    }();
-        var testFunction_2 = Modul__es6_testPublicModule;
+        return Modul__es6_testPublicModule;
+    };
         var exports = {
         default: Module_1,
         simpleArrayFunction: testFunction_1,
-        removeArrayDuplicates: testFunction_2
+        removeArrayDuplicates: Modul__es6_testPublicModule_func()
     };
     return exports;
 });
