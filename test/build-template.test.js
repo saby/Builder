f392bd431ae28b973060931770dbb78ad2b76632
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-08-28 19:20:26 +0300

    xhtml build is now working on View/Compiler API instead of custom implementation in builder https://online.sbis.ru/opendoc.html?guid=7815e62f-bdde-437c-81e6-e9dc81f4ca13

--- test/build-template.test.js
+++ test/build-template.test.js
@@ -37,4 +37,36 @@ describe('build template', () => {
       );
       testResults(result);
    });
+   it('basic tmpl', async() => {
+      let localization = true;
+      const testResults = (result) => {
+         result.text.startsWith('define(\'wml!TestModule/TestWml\'').should.equal(true);
+         result.nodeName.should.equal('wml!TestModule/TestWml');
+         result.config.should.deep.equal({
+            fileName: 'TestModule/TestWml.wml',
+            fromBuilderTmpl: true,
+            createResultDictionary: true,
+            componentsProperties: 'path/to/components-properties.json',
+            generateCodeForTranslations: localization
+         });
+      };
+
+      let result = await processingTmpl.buildTmpl(
+         '<div>{{1+1}}</div>',
+         path.normalize('TestModule/TestWml.wml'),
+         'path/to/components-properties.json',
+         localization
+      );
+      testResults(result);
+
+      // disable localization, after new build localization should be disabled in result
+      localization = false;
+      result = await processingTmpl.buildTmpl(
+         '<div>{{1+1}}</div>',
+         path.normalize('TestModule/TestWml.wml'),
+         'path/to/components-properties.json',
+         localization
+      );
+      testResults(result);
+   });
 });
