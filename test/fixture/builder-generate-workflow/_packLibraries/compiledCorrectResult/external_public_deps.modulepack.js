5de53ffacc68eb5f93a8779276165456e0591c0e
Колбешин Ф.А <fa.kolbeshin@tensor.ru>
2020-12-07 12:28:48 +0300

    Removed lazy libraries initialize https://online.sbis.ru/opendoc.html?guid=91fc8ca7-8e9c-4def-bd7d-15bde9c92659

--- test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/external_public_deps.modulepack.js
+++ test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/external_public_deps.modulepack.js
@@ -5,49 +5,32 @@ define('Modul/external_public_deps', [
     'Modul/Modul',
     'Modul/publicFunction1'
 ], function (removeArrayDuplicates, require, exports, Module_1, testFunction_1) {
-        function lazyDefineProperty(scope, name, moduleName, factory) {
-        Object.defineProperty(scope, name, {
-            get: function () {
-                var e = factory();
-                if ('function' === typeof e && e.prototype && !e.prototype.hasOwnProperty('_moduleName'))
-                    e.prototype._moduleName = moduleName;
-                return e;
-            },
-            enumerable: true
-        });
-    }
         exports['Modul/_es6/testPublicModule'] = true;
-        var Modul__es6_testPublicModule;
-    var Modul__es6_testPublicModule_func = function () {
-        if (!Modul__es6_testPublicModule) {
-            Modul__es6_testPublicModule = function () {
-                'use strict';
-                var exports = {};
-                var result = function (require, exports, removeArrayDuplicates) {
-                    'use strict';
-                    return removeArrayDuplicates;
-                }(require, exports, removeArrayDuplicates);
-                if (result instanceof Function) {
-                    return result;
-                } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
-                    return result;
-                } else {
-                    for (var property in result) {
-                        if (result.hasOwnProperty(property)) {
-                            exports[property] = result[property];
-                        }
-                    }
+        var Modul__es6_testPublicModule = function () {
+        'use strict';
+        var exports = {};
+        var result = function (require, exports, removeArrayDuplicates) {
+            'use strict';
+            return removeArrayDuplicates;
+        }(require, exports, removeArrayDuplicates);
+        if (result instanceof Function) {
+            return result;
+        } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
+            return result;
+        } else {
+            for (var property in result) {
+                if (result.hasOwnProperty(property)) {
+                    exports[property] = result[property];
                 }
-                return exports;
-            }();
+            }
         }
-        return Modul__es6_testPublicModule;
-    };
+        return exports;
+    }();
+        var testFunction_2 = Modul__es6_testPublicModule;
         var exports = {
         default: Module_1,
         simpleArrayFunction: testFunction_1,
-        removeArrayDuplicates: Modul__es6_testPublicModule_func()
+        removeArrayDuplicates: testFunction_2
     };
-    exports._packedLibrary = true;
     return exports;
 });
--- test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/testNativeNamesImports.modulepack.js
+++ test/fixture/builder-generate-workflow/_packLibraries/compiledCorrectResult/testNativeNamesImports.modulepack.js
@@ -2,53 +2,34 @@ define('Modul/testNativeNamesImports', [
     'require',
     'exports'
 ], function (require, exports) {
-        function lazyDefineProperty(scope, name, moduleName, factory) {
-        Object.defineProperty(scope, name, {
-            get: function () {
-                var e = factory();
-                if ('function' === typeof e && e.prototype && !e.prototype.hasOwnProperty('_moduleName'))
-                    e.prototype._moduleName = moduleName;
-                return e;
-            },
-            enumerable: true
-        });
-    }
     Object.defineProperty(exports, '__esModule', { value: true });
         exports['Modul/_es6/fetch'] = true;
-        var Modul__es6_fetch;
-    var Modul__es6_fetch_func = function () {
-        if (!Modul__es6_fetch) {
-            Modul__es6_fetch = function () {
-                'use strict';
-                var exports = {};
-                var result = function (require, exports) {
-                    'use strict';
-                    Object.defineProperty(exports, '__esModule', { value: true });
-                    function someTest() {
-                        var test1 = 'Тестовое сообщение';
-                        return test1;
-                    }
-                    exports.someTest = someTest;
-                }(require, exports);
-                if (result instanceof Function) {
-                    return result;
-                } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
-                    return result;
-                } else {
-                    for (var property in result) {
-                        if (result.hasOwnProperty(property)) {
-                            exports[property] = result[property];
-                        }
-                    }
+        var Modul__es6_fetch = function () {
+        'use strict';
+        var exports = {};
+        var result = function (require, exports) {
+            'use strict';
+            Object.defineProperty(exports, '__esModule', { value: true });
+            function someTest() {
+                var test1 = 'Тестовое сообщение';
+                return test1;
+            }
+            exports.someTest = someTest;
+        }(require, exports);
+        if (result instanceof Function) {
+            return result;
+        } else if (result && Object.getPrototypeOf(result) !== Object.prototype) {
+            return result;
+        } else {
+            for (var property in result) {
+                if (result.hasOwnProperty(property)) {
+                    exports[property] = result[property];
                 }
-                return exports;
-            }();
+            }
         }
-        return Modul__es6_fetch;
-    };
-    lazyDefineProperty(exports, 'fetch', 'Modul/testNativeNamesImports:fetch', function () {
-        return Modul__es6_fetch_func();
-    });
-    exports._packedLibrary = true;
+        return exports;
+    }();
+    exports.fetch = Modul__es6_fetch;
+    
     return exports;
 });
