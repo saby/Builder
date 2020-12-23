35a41041cf2752cbea5ce29b2c5222fd58c5a404
Колбешин Ф.А <fa.kolbeshin@tensor.ru>
2020-12-23 21:53:08 +0300

    fixed an issue in lazy package generator when internal module with plugin(e.g. optional) could be required as external module and create a cycle from superbundle to itself https://online.sbis.ru/opendoc.html?guid=a07a55d5-d2ce-49cf-be69-a727fe927662

--- test/fixture/custompack/correctResult/lazyPrivatePackage.js
+++ test/fixture/custompack/correctResult/lazyPrivatePackage.js
@@ -1,5 +1,7 @@
 (function () {var bundleExports = {};
 require(["require"],function(require) {
+var Modul_private;
+Object.defineProperty(bundleExports, 'Modul/private', {get: function() {if (!Modul_private) {Modul_private = {};var result = function(e,o){return{_moduleName:'Modul/private',externalDeps:{module1:e,module2:o}};}(InterfaceModule1_amdModule,InterfaceModule2_amdModule);if (result) {Modul_private = result;}}return Modul_private;},enumerable: true});
 var InterfaceModule2_amdModule;
 Object.defineProperty(bundleExports, 'InterfaceModule2/amdModule', {get: function() {if (!InterfaceModule2_amdModule) {InterfaceModule2_amdModule = {};var result = function(){return{_moduleName:'InterfaceModule1/amdModule'};}();if (result) {InterfaceModule2_amdModule = result;}}return InterfaceModule2_amdModule;},enumerable: true});
 var InterfaceModule1_library;
@@ -11,6 +13,7 @@ Object.defineProperty(bundleExports, 'InterfaceModule1/_private/module2', {get: 
 var InterfaceModule1__private_module1;
 Object.defineProperty(bundleExports, 'InterfaceModule1/_private/module1', {get: function() {if (!InterfaceModule1__private_module1) {InterfaceModule1__private_module1 = {};var result = function(e,t){'use strict';Object.defineProperty(t,'__esModule',{value:true});var r=function(){function e(e){this.variables=e;}return e;}();t.default=r;}(require,InterfaceModule1__private_module1);if (result) {InterfaceModule1__private_module1 = result;}}return InterfaceModule1__private_module1;},enumerable: true});
 (function(){define('css!InterfaceModule2/moduleStyle',['css!Modul/lazy-private.package'],'');define('css!InterfaceModule1/moduleStyle',['css!Modul/lazy-private.package'],'');define('css!InterfaceModule1/amdModule',['css!Modul/lazy-private.package'],'');})();
+define('Modul/private',  function() {return bundleExports['Modul/private'];});
 define('InterfaceModule2/amdModule', ['css!InterfaceModule2/amdModule'],  function() {return bundleExports['InterfaceModule2/amdModule'];});
 define('InterfaceModule1/library',  function() {return bundleExports['InterfaceModule1/library'];});
 define('InterfaceModule1/amdModule', ['css!InterfaceModule1/amdModule'],  function() {return bundleExports['InterfaceModule1/amdModule'];});
--- test/fixture/custompack/Модуль/private.js
+++ test/fixture/custompack/Модуль/private.js
@@ -1,1 +1,1 @@
-define('Modul/private', [], function(){return {_moduleName: 'Modul/private'};});
+define('Modul/private', ['optional!InterfaceModule1/amdModule', 'optional!InterfaceModule2/amdModule'], function(module1, module2){return {_moduleName: 'Modul/private', externalDeps: { module1: module1, module2: module2 }};});
