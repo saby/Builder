2b7a1506bdb4879f08be8e4a33e63f8519aa6841
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2021-04-20 10:06:58 +0300

    fixed an issue when cdn and versions meta data saves into cache with absolute paths instead of relative paths https://online.sbis.ru/opendoc.html?guid=097afec5-580c-4fac-bba3-2dc754dddbcb

--- gulp/builder/plugins/create-versioned-modules.js
+++ gulp/builder/plugins/create-versioned-modules.js
@@ -18,6 +18,9 @@ const through = require('through2'),
  * @returns {stream}
  */
 module.exports = function declarePlugin(taskParameters, moduleInfo) {
+   const prettyCacheModulePath = helpers.unixifyPath(moduleInfo.output);
+   const prettyModulePath = helpers.unixifyPath(moduleInfo.path);
+   const currentModuleName = helpers.unixifyPath(moduleInfo.output).split('/').pop();
    return through.obj(
       function onTransform(file, encoding, callback) {
          const startTime = Date.now();
@@ -29,10 +32,14 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
          if (file.versioned && (file.basename.endsWith('.html') || file.basename.endsWith(`.min${file.extname}`))) {
             let relativeFilePath = path.relative(moduleInfo.path, file.history[0]);
             relativeFilePath = path.join(path.basename(moduleInfo.path), relativeFilePath);
-            moduleInfo.cache.storeVersionedModule(
-               relativeFilePath,
-               transliterate(file.history[file.history.length - 1])
+            const prettyFilePath = helpers.unixifyPath(transliterate(file.history[file.history.length - 1]));
+            const isSourcePath = prettyFilePath.includes(prettyModulePath);
+            let relativeOutputPath = path.relative(
+               isSourcePath ? prettyModulePath : prettyCacheModulePath,
+               prettyFilePath
             );
+            relativeOutputPath = helpers.unixifyPath(path.join(currentModuleName, relativeOutputPath));
+            moduleInfo.cache.storeVersionedModule(relativeFilePath, relativeOutputPath);
          }
          callback(null, file);
          taskParameters.storePluginTime('presentation service meta', startTime);
@@ -44,39 +51,28 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
          try {
             const versionedModules = [];
             const versionCache = moduleInfo.cache.getVersionedModulesCache(moduleInfo.name);
-            const prettyCacheModulePath = helpers.prettifyPath(transliterate(moduleInfo.output));
-            const prettyModulePath = helpers.prettifyPath(transliterate(moduleInfo.path));
-            const currentModuleName = helpers.prettifyPath(moduleInfo.output).split('/').pop();
             Object.keys(versionCache).forEach((currentModule) => {
                versionedModules.push(...versionCache[currentModule]);
             });
-            const versionedModulesPaths = versionedModules.map((currentFile) => {
-               const
-                  prettyFilePath = transliterate(helpers.prettifyPath(currentFile)),
-                  isSourcePath = prettyFilePath.includes(prettyModulePath),
-                  relativePath = path.relative(isSourcePath ? prettyModulePath : prettyCacheModulePath, prettyFilePath);
-
-               return helpers.unixifyPath(path.join(currentModuleName, relativePath));
-            });
 
             if (taskParameters.config.contents) {
-               versionedModulesPaths.push(`${currentModuleName}/contents.json`);
+               versionedModules.push(`${currentModuleName}/contents.json`);
 
                // in desktop apps there will not be any contents.js files(debug files
                // removes from output in desktop apps). Write it in versioned_modules
                // for online projects only
                if (taskParameters.config.sources) {
-                  versionedModulesPaths.push(`${currentModuleName}/contents.json.js`);
+                  versionedModules.push(`${currentModuleName}/contents.json.js`);
                }
                if (taskParameters.config.minimize) {
-                  versionedModulesPaths.push(`${currentModuleName}/contents.min.json`);
-                  versionedModulesPaths.push(`${currentModuleName}/contents.json.min.js`);
+                  versionedModules.push(`${currentModuleName}/contents.min.json`);
+                  versionedModules.push(`${currentModuleName}/contents.json.min.js`);
                }
             }
 
             const file = new Vinyl({
                path: '.builder/versioned_modules.json',
-               contents: Buffer.from(JSON.stringify(versionedModulesPaths.sort())),
+               contents: Buffer.from(JSON.stringify(versionedModules.sort())),
                moduleInfo
             });
             this.push(file);
@@ -85,10 +81,10 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
              * оставляем версионированные модули, могут пригодиться в дальнейшем при паковке
              * @type {string[]}
              */
-            taskParameters.setVersionedModules(currentModuleName, versionedModulesPaths);
+            taskParameters.setVersionedModules(currentModuleName, versionedModules);
          } catch (error) {
             logger.error({
-               message: "Ошибка Builder'а",
+               message: "Builder's error during versioned_modules meta generating",
                error,
                moduleInfo
             });
