f6d24cfe45d6fbb5cbcdedddf2b8482aebb45fc2
fedor_kolbeshin <fa.kolbeshin@tensor.ru>
2021-04-25 15:52:53 +0300

    added packing of svg https://online.sbis.ru/opendoc.html?guid=e4bbc071-ad30-4146-b0e9-916c17f69df2

--- gulp/builder/plugins/process-svg.js
+++ gulp/builder/plugins/process-svg.js
@@ -8,8 +8,11 @@ const through = require('through2'),
    transliterate = require('../../../lib/transliterate'),
    Vinyl = require('vinyl'),
    logger = require('../../../lib/logger').logger();
-const {optimize, extendDefaultPlugins} = require('svgo');
+const { optimize, extendDefaultPlugins } = require('svgo');
 const path = require('path');
+
+// allowed icon name template
+const ICON_NAME_REGEX = /^icon-[A-Za-z0-9]+$/;
 
 const postProcessSVG = (ast, params) => {
    if (ast.name === 'svg') {
@@ -18,7 +21,8 @@ const postProcessSVG = (ast, params) => {
    }
    delete ast.attributes.xmlns;
 };
-const getSvgoOptions = (fileName) => {
+
+function getSvgoOptions(fileName) {
    return {
       plugins: extendDefaultPlugins([
          {
@@ -51,6 +55,7 @@ const getSvgoOptions = (fileName) => {
       ])
    };
 }
+
 /**
  * Объявление плагина
  * @param {TaskParameters} taskParameters параметры для задач
@@ -60,8 +65,7 @@ const getSvgoOptions = (fileName) => {
 module.exports = function declarePlugin(taskParameters, moduleInfo) {
    const packagesToBuild = [];
    return through.obj(
-      /* @this Stream */
-      async function onTransform(file, encoding, callback) {
+      function onTransform(file, encoding, callback) {
          try {
             if (!file.contents) {
                callback();
@@ -73,18 +77,38 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
                return;
             }
 
+            // throw an error if svg icon name isn't matching our allowed pattern
+            if (!ICON_NAME_REGEX.test(file.stem)) {
+               logger.error({
+                  message: `Svg name "${file.stem}" is forbidden. Use a name matching the pattern /^icon-[A-Za-z0-9]+$/`,
+                  filePath: file.history[0],
+                  moduleInfo
+               });
+               taskParameters.cache.markFileAsFailed(file.history[0]);
+               callback(null, file);
+               return;
+            }
+
             let relativeFilePath = path.relative(moduleInfo.path, file.history[0]);
             relativeFilePath = path.join(
                path.basename(moduleInfo.path),
                relativeFilePath
             );
             const outputPath = path.join(moduleInfo.output, transliterate(relativeFilePath));
+            const nameParts = relativeFilePath.split(path.sep);
             if (file.cached) {
                taskParameters.cache.addOutputFile(file.history[0], outputPath, moduleInfo);
+
+               // add package artifact to output meta of current svg to remove it with a garbage
+               // collector if all of current package members are removed from sources
+               taskParameters.cache.addOutputFile(
+                  file.history[0],
+                  path.join(moduleInfo.output, `${nameParts[1]}.svg`),
+                  moduleInfo
+               );
                callback(null, file);
                return;
             }
-            const nameParts = relativeFilePath.split(path.sep);
             if (nameParts.length >= 3) {
                packagesToBuild.push(nameParts[1]);
             }
@@ -93,7 +117,7 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
          } catch (error) {
             taskParameters.cache.markFileAsFailed(file.history[0]);
             logger.error({
-               message: "Ошибка builder'а при компиляции в JS",
+               message: "Builder's error during processing svg image",
                error,
                moduleInfo,
                filePath: file.history[0]
@@ -102,19 +126,28 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
          callback(null, file);
       },
 
+      /* @this Stream */
       function onFlush(callback) {
          const currentPackagesMeta = moduleInfo.cache.getCurrentSvgPackagesMeta();
          const lastPackagesMeta = moduleInfo.cache.getLastSvgPackagesMeta();
          Object.keys(currentPackagesMeta).forEach((currentPackage) => {
             if (
                packagesToBuild.includes(currentPackage) ||
-               lastPackagesMeta[currentPackage] && lastPackagesMeta[currentPackage].length !== currentPackagesMeta[currentPackage].length
+               (lastPackagesMeta[currentPackage] &&
+               lastPackagesMeta[currentPackage].length !== currentPackagesMeta[currentPackage].length)
             ) {
                const currentResult = ['<svg xmlns="http://www.w3.org/2000/svg">'];
+               const currentPackageDependencies = [];
+               const packageName = `${path.basename(moduleInfo.output)}/${currentPackage}.svg`;
                currentPackagesMeta[currentPackage].forEach((currentSvg) => {
+                  currentPackageDependencies.push(currentSvg.path);
                   const optimizedSvg = optimize(currentSvg.content, getSvgoOptions(path.basename(currentSvg.path, '.svg')));
                   currentResult.push(optimizedSvg.data);
-               })
+
+                  // add package artifact to output meta of current svg to remove it with a garbage
+                  // collector if all of current package members are removed from sources
+                  taskParameters.cache.addOutputFile(currentSvg.path, packageName, moduleInfo);
+               });
                currentResult.push('</svg>');
                this.push(
                   new Vinyl({
